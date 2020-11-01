871cf0b2ac86ce60de6cc2da37d3dc1368647ad2
Abby Howe <howe.abigail@gmail.com>
2020-11-01 16:06:03 -0600

    Exeption for 11.4.1. Divide By Zero!

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,18 +7,34 @@ namespace csharp_web_dev_lsn9exceptions
     {
         static double Divide(double x, double y)
         {
-            // Write your code here!
+
+            if (y == 0)
+            {
+                throw new ArgumentOutOfRangeException("Value is 0");
+            }
+            return x / y;
+         
         }
 
-        static int CheckFileExtension(string fileName)
-        {
-            // Write your code here!
-        }
+        //static int CheckFileExtension(string fileName)
+        //{
+        //    // Write your code here!
+        //}
 
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+
+            try
+            {
+                Console.WriteLine(Divide(80, 0));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Y can not be equal to zero");
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
