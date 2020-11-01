297625d1180b2fc2e6d1764c0d1e128e54e858a6
Abby Howe <howe.abigail@gmail.com>
2020-11-01 17:28:01 -0600

    11.4.2. Test Student Labs Completed

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -16,10 +16,27 @@ namespace csharp_web_dev_lsn9exceptions
          
         }
 
-        //static int CheckFileExtension(string fileName)
-        //{
-        //    // Write your code here!
-        //}
+        static int CheckFileExtension(string fileName)
+        {
+            // Write your code here!
+            int StudentCSharpScore= 0;
+            //string FileName = fileName; 
+            string Substr = fileName.Substring(fileName.Length - 3);
+
+            if (Substr == ".cs")
+            {
+
+                StudentCSharpScore = +1;
+                Console.WriteLine(fileName + " Substring is: " + Substr + "file type  score is: " + StudentCSharpScore );
+                return StudentCSharpScore;
+            }
+            else if(Substr == "" || Substr.Equals(null))
+       
+            StudentCSharpScore = +0;
+            Console.WriteLine(fileName + " Substring is: " + Substr + "file type  score is: " + StudentCSharpScore);
+            throw new ArgumentNullException("Must submit a file with a .cs filetype.");
+            return StudentCSharpScore;
+        }
 
 
         static void Main(string[] args)
@@ -28,7 +45,7 @@ namespace csharp_web_dev_lsn9exceptions
 
             try
             {
-                Console.WriteLine(Divide(80, 0));
+                Console.WriteLine(Divide(80, 100));
             }
             catch (ArgumentOutOfRangeException ex)
             {
@@ -42,6 +59,21 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+
+            foreach (var (key, value) in students)
+            {
+                try
+                {
+                    Console.WriteLine($"{key} : {value}");
+                    string fileName = value;
+                    CheckFileExtension(fileName);
+                }
+                catch
+                {
+                    Console.WriteLine($"{key} : File is not a C# type or is empty. \n Score is 0");
+                }
+                
+            }
 
 
         }
